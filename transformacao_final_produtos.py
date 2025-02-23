63f08d93eb0978644eaebb19ecd37f57ce28fb2e
Yugo Ueda <renanyugo@hotmail.com>
2025-02-23 20:16:27 -0300

    Arrumado o problema das aspas

--- transformacao_final_produtos.py
+++ transformacao_final_produtos.py
@@ -5,27 +5,19 @@ import csv
 
 # Somente o campo nome_produto possui aspas duplas, então removemos elas e as vírgulas dentro dessas aspas.
 # Por exemplo, "Produto, teste" vira Produto teste.
-def remover_aspas_produtos(nome_arquivo):
-    nome_corrigido = None
+def remover_aspas_produtos(arquivo_entrada, arquivo_saida):
     count = 0
-    with open(nome_arquivo, 'r', newline='', encoding='utf-8') as arquivo_produtos:
+    with open(arquivo_entrada, 'r', newline='', encoding='utf-8') as arquivo_produtos:
         reader = csv.reader(arquivo_produtos)
-        with open('produtos_final.csv', 'w', newline='', encoding='utf-8') as arquivo_final:
-            writer = csv.writer(arquivo_final)
+        with open(arquivo_saida, 'w', newline='', encoding='utf-8') as arquivo_saida:
+            writer = csv.writer(arquivo_saida)
             for linha in reader:
-                nome_produto = linha[0]
-                # Remove aspas duplas do nome
-                if nome_produto.startswith('"') and nome_produto.endswith('"'):
-                    nome_corrigido = nome_produto[1:-1]
-                    # Remove vírgulas dentro do nome
-                    nome_corrigido = nome_corrigido.replace(',', '')
-                    writer.writerow([nome_corrigido, linha[1], linha[2], linha[3], linha[4], linha[5]])
-                    count += 1
-                    print(f"Corrigidos: {count} Produto {nome_corrigido}, {linha[1]}, {linha[2]}, {linha[3]}, {linha[4]}, {linha[5]}")
-                else:
-                    writer.writerow([nome_produto, linha[1], linha[2], linha[3], linha[4], linha[5]])
-                    count += 1
-                    print(f"Corrigidos: {count} Produto {nome_corrigido}, {linha[1]}, {linha[2]}, {linha[3]}, {linha[4]}, {linha[5]}")
+                nome_produto_corrigido = linha[0]       
+                # As aspas duplas já são automaticamente removidas pelo Python CSV, então só precisamos remover as vírgulas.
+                linha[0] = nome_produto_corrigido.replace(',', '')
+                writer.writerow(linha)
+                count += 1
+                print(f'Corrigidos {count}: {linha}')
 
 if __name__ == '__main__':
-    remover_aspas_produtos('produtos_intermediario.csv')
+    remover_aspas_produtos('produtos_intermediario.csv', 'produtos_final.csv')
