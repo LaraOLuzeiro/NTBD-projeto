e2f94964a5ba5d5b117baf842c008dc01ccc1ae0
unknown <daniellayuka@estudante.ufscar.br>
2025-02-25 01:35:27 -0300

    Modificações no insere_local

--- insere_local.py
+++ insere_local.py
@@ -4,21 +4,21 @@ import pandas as pd
 import psycopg2
 
 # Função para mapear o código da UF para a região
-def get_region_by_code(uf_code):
-    if uf_code.startswith('1'):
+def atribui_regiao(cod):
+    if cod.startswith('1'):
         return "Norte"
-    elif uf_code.startswith('2'):
+    elif cod.startswith('2'):
         return "Nordeste"
-    elif uf_code.startswith('3'):
+    elif cod.startswith('3'):
         return "Sudeste"
-    elif uf_code.startswith('4'):
+    elif cod.startswith('4'):
         return "Sul"
-    elif uf_code.startswith('5'):
+    elif cod.startswith('5'):
         return "Centro-Oeste"
     return "Desconhecido"  # Caso o código não se encaixe em nenhum critério
 
 # Função para fazer o scraping dos dados do IBGE
-def scrape_ibge_states():
+def scraper_ibge():
     url = "https://www.ibge.gov.br/explica/codigos-dos-municipios.php"
     response = requests.get(url)
     response.raise_for_status()  
@@ -26,30 +26,29 @@ def scrape_ibge_states():
     soup = BeautifulSoup(response.text, "html.parser")
     table = soup.find("tbody", class_="codigos-list")
     
-    states_data = []
+    estados = []
     
     if table:
         rows = table.find_all("tr", class_="uf data-line")
         for row in rows:
             cols = row.find_all("td")
-            state_name = cols[0].text.strip()
-            uf_code = cols[1].text.strip().split("ver municípios")[0].strip()
-            uf_abbr = cols[0].find("a")["href"].replace("#", "")
-            region = get_region_by_code(uf_code)
-            states_data.append({
-                "estado": state_name,
-                "regiao": region,
+            nome_estado = cols[0].text.strip()
+            codigo = cols[1].text.strip().split("ver municípios")[0].strip()
+            regiao = atribui_regiao(codigo)
+            estados.append({
+                "estado": nome_estado,
+                "regiao": regiao,
                 "pais": "Brasil"
             })
 
     # Criando um DataFrame com os dados extraídos
-    df = pd.DataFrame(states_data)
+    df = pd.DataFrame(estados)
     return df
 
 # Função para inserir os dados no banco de dados
-def generate_insert_statements(df, table_name):
+def inserts(df, table_name):
     # Lista para armazenar os comandos INSERT
-    insert_statements = []
+    inserts = []
 
     # Itera sobre as linhas do DataFrame
     for index, row in df.iterrows():
@@ -57,14 +56,14 @@ def generate_insert_statements(df, table_name):
         values = ', '.join([f"'{str(value)}'" for value in row])
         
         # Cria o comando INSERT INTO
-        insert_statement = f"INSERT INTO {table_name} ({', '.join(df.columns)}) VALUES ({values});"
+        insert = f"INSERT INTO {table_name} ({', '.join(df.columns)}) VALUES ({values});"
         
         # Adiciona o comando à lista
-        insert_statements.append(insert_statement)
+        inserts.append(insert)
     
-    return insert_statements
+    return inserts
 
-def insert_data_into_db(df, table_name):
+def inserir_bd(df, table_name):
     # Conexão com o banco de dados PostgreSQL
     connection_string = "postgresql://postgres:SUA_SENHA@localhost:5432/Conab_DW"
     
@@ -73,10 +72,10 @@ def insert_data_into_db(df, table_name):
     cursor = conn.cursor()
 
     # Gerar os comandos INSERT  
-    insert_statements = generate_insert_statements(df, table_name)
+    insert = inserts(df, table_name)
 
     # Executar cada comando INSERT
-    for statement in insert_statements:
+    for statement in insert:
         try:
             cursor.execute(statement)
             conn.commit()  # Confirma a transação
@@ -84,15 +83,15 @@ def insert_data_into_db(df, table_name):
             print(f"Erro ao inserir dados: {e}")
             conn.rollback()  # Desfaz a transação em caso de erro
 
-    # Fechar a conexão
+    # Fecha a conexão
     cursor.close()
     conn.close()
 
 if __name__ == "__main__":
     # Scrape dos dados
-    df = scrape_ibge_states()
+    df = scraper_ibge()
 
     # Inserção no banco de dados
-    insert_data_into_db(df, 'Dimensao_Local')
+    inserir_bd(df, 'Dimensao_Local')
     
     print("Dados inseridos com sucesso!")
